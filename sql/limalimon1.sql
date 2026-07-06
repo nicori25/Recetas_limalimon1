757302aad491ccba307b1d513d5a7e49dce9e45f
nicori25 <ariaranda07@gmail.com>
2026-07-06 17:26:31 -0300

    agrego de campo rol en usuarios para los administradores

--- sql/limalimon1.sql
+++ sql/limalimon1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-05-2026 a las 22:08:55
+-- Tiempo de generación: 06-07-2026 a las 22:22:50
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `limalimon1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `receta_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `favoritos`
+--
+
+INSERT INTO `favoritos` (`id`, `usuario_id`, `receta_id`) VALUES
+(3, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -48,8 +67,16 @@ CREATE TABLE `recetas` (
   `ingredientes` text DEFAULT NULL,
   `pasos` text DEFAULT NULL,
   `tipo` varchar(150) DEFAULT NULL,
-  `estacion` varchar(50) DEFAULT NULL
+  `estacion` varchar(50) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `recetas`
+--
+
+INSERT INTO `recetas` (`id`, `titulo`, `descripcion`, `usuario_id`, `ingredientes`, `pasos`, `tipo`, `estacion`, `imagen`) VALUES
+(2, 'Budin de banana', 'hola prueba', 1, 'Ingredientes:\r\n2 bananas\r\n2 huevos\r\n200 gr de garina leudante\r\n100 ml de leche sin lactosa\r\n100 gr de azucar\r\n1 cucharadita de polvo para hornear\r\n1 cucharadita de esencia de vainilla\r\n', '1. precalentar el horno a 180°C\r\n2. pisar las bananas en un bowl\r\n3. agregar huevos, azucar y vainilla\r\n4. incorporar la leche sin lactosa\r\n5. agregar harina y polvo de hornear\r\n6. mezclar hasta integrar\r\n7. colocar en un molde aceitado\r\n8. hornear durante 40 minutos aprox. ', 'sin lactosa', 'otoño', '1783076237_6a47958dc277c.jpg');
 
 -- --------------------------------------------------------
 
@@ -62,19 +89,50 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `preferencia_tipo` varchar(150) DEFAULT NULL
+  `preferencia_tipo` varchar(150) DEFAULT NULL,
+  `rol` varchar(20) NOT NULL DEFAULT 'usuario'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `preferencia_tipo`) VALUES
-(1, 'ariel', 'ariaranda07@gmail.com', '$2y$10$/jnPZhyhCV1ve18eiTNT9ewV.O6GInQlgARSW5FIZsg4BLPLS2CkC', NULL);
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `preferencia_tipo`, `rol`) VALUES
+(1, 'ariel', 'ariaranda07@gmail.com', '$2y$10$2AytYvh3fqGd/HhCZ.kQbOg.mKovSJI3vI38VRoyNtZg4mdMs1VgK', 'sin mani', 'admin'),
+(4, 'niko', 'aria@gmail.com', '$2y$10$aHlws5LMwZZqDLNZCjMjYucXii3x0SNTE7FXxBuFLrhN9QU6rrah2', NULL, 'usuario'),
+(5, 'nikolas', 'niko@gmail.com', '$2y$10$6CeOtu2Em4Ieyrl6lLXo.../8faRL.Hs8cMWKFL.3lV9cBATHSkqW', NULL, 'usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `valoraciones`
+--
+
+CREATE TABLE `valoraciones` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `receta_id` int(11) NOT NULL,
+  `puntuacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `valoraciones`
+--
+
+INSERT INTO `valoraciones` (`id`, `usuario_id`, `receta_id`, `puntuacion`) VALUES
+(3, 1, 2, 5);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `receta_id` (`receta_id`);
 
 --
 -- Indices de la tabla `opiniones`
@@ -99,30 +157,57 @@ ALTER TABLE `usuarios`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indices de la tabla `valoraciones`
+--
+ALTER TABLE `valoraciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `receta_id` (`receta_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `opiniones`
 --
 ALTER TABLE `opiniones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `recetas`
 --
 ALTER TABLE `recetas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `valoraciones`
+--
+ALTER TABLE `valoraciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`receta_id`) REFERENCES `recetas` (`id`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `opiniones`
@@ -136,6 +221,13 @@ ALTER TABLE `opiniones`
 --
 ALTER TABLE `recetas`
   ADD CONSTRAINT `recetas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `valoraciones`
+--
+ALTER TABLE `valoraciones`
+  ADD CONSTRAINT `valoraciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `valoraciones_ibfk_2` FOREIGN KEY (`receta_id`) REFERENCES `recetas` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
